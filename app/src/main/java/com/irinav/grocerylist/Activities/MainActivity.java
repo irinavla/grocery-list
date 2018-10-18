46b5f723251a5aa8dfb76bb14d6caf8b23fe3208
Irina Vlasceanu <irinav0402@gmail.com>
2018-10-18 12:12:22 +0300

    build Enter item dialog view

---
+++ app/src/main/java/com/irinav/grocerylist/Activities/MainActivity.java
@@ -0,0 +1,54 @@
+package com.irinav.grocerylist.Activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.irinav.grocerylist.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private EditText groceryItem;
+    private EditText quantity;
+    private Button saveButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                createPopupDialog();
+            }
+        });
+    }
+
+    private void createPopupDialog() {
+        dialogBuilder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.popup, null);
+
+        groceryItem = (EditText) view.findViewById(R.id.groceryItem);
+        quantity = (EditText) view.findViewById(R.id.groceryQty);
+        saveButton = (Button) view.findViewById(R.id.saveButton);
+
+        dialogBuilder.setView(view);
+        dialog = dialogBuilder.create();
+        dialog.show();
+    }
+
+}
